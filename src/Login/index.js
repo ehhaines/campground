8e168cd11dbac33953e8e6845cdb444eec08417c
ehhaines <haines.e@northeastern.edu>
2022-11-23 04:54:55 -0500

    Create login and sign up components

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,28 +1,37 @@
 import React from "react";
+import Signin from "./signin";
+import Signup from "./signup";
 import chooseBackground from "../background";
 
 const LoginComponent = () => {
   const currentBackground = chooseBackground();
   let isLogin = true;
   return(
-    <div className="eh-center w-100 eh-background" style={{
-      backgroundImage: `url("/images/${currentBackground.image}")`
-    }}>
-      <div className='w-50'>
-        <ul className="nav nav-tabs">
-          <li className="nav-item">
-            <div className={`nav-link text-dark ${isLogin ? "active" : "bg-primary"}`}>Login</div>
-          </li>
-          <li className="nav-item">
-            <div className={`nav-link text-dark ${!isLogin ? "active" : "bg-primary"}`}>Sign up</div>
-          </li>
-        </ul>
-      </div>
-      <div className="fixed-bottom mb-3 ms-3 p-2 fs-6 bg-warning eh-trim rounded">
-        {currentBackground.park}
+    <div>
+      <div className="eh-center w-100 eh-background" style={{
+        backgroundImage: `url("/images/${currentBackground.image}")`
+      }}>
+        <div className="w-50">
+          <div>
+            <ul className="nav nav-tabs">
+              <li className="nav-item">
+                <div className={`nav-link text-dark ${isLogin ? "active border-white" : "bg-secondary"}`}>Login</div>
+              </li>
+              <li className="nav-item">
+                <div className={`nav-link text-dark ${!isLogin ? "active border-white" : "bg-secondary"}`}>Sign up</div>
+              </li>
+            </ul>
+          </div>
+          <div>
+            {isLogin && <Signin/>}
+            {!isLogin && <Signup/>}
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
+
 
 export default LoginComponent;
