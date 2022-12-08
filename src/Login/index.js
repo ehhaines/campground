696f2c4d5628cab188eb4029bfb6c061d036b189
Forestocean37 <forestocean37@gmail.com>
2022-12-08 00:50:04 -0800

    update

--- src/Login/index.js
+++ src/Login/index.js
@@ -6,35 +6,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const LoginComponent = () => {
-  const [isLogin, setLogin] = useState(true);
-  const nav = useNavigate();
-  return(
-    <div>
-      <button className="position-absolute btn ms-3 mt-3 bg-dark" onClick={() => nav("/")}>
-        <i><FontAwesomeIcon icon={faArrowLeft} color="white"/></i>
-      </button>
-      <div className="eh-center w-100 eh-background" style={{
-        backgroundImage: `url("/images/canyonlands.jpg")`
-      }}>
-        <div className="w-50 h-75 overflow-auto">
-          <div>
-            <ul className="nav nav-tabs">
-              <li className="nav-item" onClick={() => setLogin(true)}>
-                <button className={`nav-link text-dark ${isLogin ? "active border-white" : "bg-secondary"}`}>Login</button>
-              </li>
-              <li className="nav-item" onClick={() => setLogin(false)}>
-                <div className={`nav-link text-dark ${!isLogin ? "active border-white" : "bg-secondary"}`}>Sign up</div>
-              </li>
-            </ul>
-          </div>
-          <div>
-            {isLogin && <Signin/>}
-            {!isLogin && <Signup/>}
-          </div>
+    const [isLogin, setLogin] = useState(true);
+    const nav = useNavigate();
+    return(
+        <div>
+            <button className="position-absolute btn ms-3 mt-3 bg-dark" onClick={() => nav("/")}>
+                <i><FontAwesomeIcon icon={faArrowLeft} color="white"/></i>
+            </button>
+            <div className="eh-center w-100 eh-background" style={{
+                backgroundImage: `url("/images/canyonlands.jpg")`
+            }}>
+                <div className="w-50 h-75 overflow-auto">
+                    <div>
+                        <ul className="nav nav-tabs">
+                            <li className="nav-item" onClick={() => setLogin(true)}>
+                                <button className={`nav-link text-dark ${isLogin ? "active border-white" : "bg-secondary"}`}>Login</button>
+                            </li>
+                            <li className="nav-item" onClick={() => setLogin(false)}>
+                                <div className={`nav-link text-dark ${!isLogin ? "active border-white" : "bg-secondary"}`}>Sign up</div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        {isLogin && <Signin/>}
+                        {!isLogin && <Signup/>}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 
--- src/Login/signin.js
+++ src/Login/signin.js
@@ -1,19 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {loginThunk} from "../Profile/users-thunks";
 
 const Signin = () => {
-  return(
-    <div className="bg-white rounded-bottom">
-      <div>
-        <div className="input-group mb-3 col">
-          <input type="text" className="form-control mx-3 mb-2 mt-3" aria-describedby="inputGroup-sizing-default" placeholder="Email"/>
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null)
+    const {currentUser} = useSelector((state) => state.users)
+    const dispatch = useDispatch()
+    const handleLoginBtn = () => {
+        setError(null)
+        const loginUser = {username, password}
+        dispatch(loginThunk(loginUser))
+    }
+    return(
+        <div className="bg-white rounded-bottom">
+            <div>
+                <div className="input-group mb-3 col">
+                    <input type="text" className="form-control mx-3 mb-2 mt-3" aria-describedby="inputGroup-sizing-default" placeholder="Username"
+                           value={username}
+                           onChange={(e) => {setUsername(e.target.value)}}/>
+                </div>
+                <div className="input-group mb-3 col">
+                    <input type="text" className="form-control mx-3 my-2" aria-describedby="inputGroup-sizing-default" placeholder="Password"
+                           value={password}
+                           onChange={(e) => {setPassword(e.target.value)}}/>
+                </div>
+            </div>
+            <button
+                className="btn btn-primary mx-3 mb-3 mt-2"
+                onClick={handleLoginBtn}
+            >
+                Enter
+            </button>
+            {
+                error &&
+                <div className="alert alert-danger">
+                    {error}
+                </div>
+            }
+            {
+                currentUser &&
+                <h2>Welcome {currentUser.username}</h2>
+            }
         </div>
-        <div className="input-group mb-3 col">
-          <input type="text" className="form-control mx-3 my-2" aria-describedby="inputGroup-sizing-default" placeholder="Password"/>
-        </div>
-      </div>
-      <button className="btn btn-primary mx-3 mb-3 mt-2">Enter</button>
-    </div>
-  );
+    );
 }
 
 export default Signin;
