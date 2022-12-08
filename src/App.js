65def348fd8b21e6042ff245f5cc705b6c437290
Forestocean37 <forestocean37@gmail.com>
2022-12-08 13:34:11 -0800

    profile, edit-profile

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Provider } from "react-redux";
 import LoginComponent from "./Login";
 import NavbarComponent from "./Navbar";
 import ProfileComponent from "./Profile";
+import EditProfile from "./edit-profile";
 import NpsSearch from "./nps/Search/nps-search";
 import npsReducer from "./nps/nps-reducer";
 import usersReducer from "./Profile/users-reducer";
+import profileReducer from "./reducers/profile-reducer";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router";
 import "./App.css";
@@ -15,7 +17,8 @@ import ParkComponent from "./Park";
 const store = configureStore({
   reducer: {
     nps: npsReducer,
-    users: usersReducer
+    users: usersReducer,
+    profile: profileReducer
   }
 })
 
@@ -31,6 +34,7 @@ function App() {
             <Route path="/details/:park" element={<ParkComponent/>}/>
             <Route path="/login" element={<LoginComponent/>}/>
             <Route path="/profile" element={<ProfileComponent/>}/>
+            <Route path="/edit-profile" element={<EditProfile/>}/>
           </Routes>
         </div>
       </BrowserRouter>
