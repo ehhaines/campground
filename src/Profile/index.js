f1a71659c4eaf2052c1be526e6fa3c8efe332d23
Xiaohu Li <li.xiaohu3@northeastern.edu>
2022-12-13 21:08:39 -0500

    add trips to profile

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./profile.css";
 import {useDispatch, useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 import {logoutThunk} from "./users-thunks";
+import CompletedTripsComponent from "../Trip/completed-trips";
+import {findAllTripsThunk} from "../Trip/trips-thunks";
+import LoadSVG from "../Spin-1s-200px.svg";
+import {useNavigate} from "react-router";
 
 const ProfileComponent = () => {
     const {currentUser} = useSelector(state => state.users);
+    const {trips, tripsLoading} = useSelector(state => state.trips);
+
+    const releventTrips = () => trips.filter(trip => trip.user === currentUser._id);
+    const completedTrips = () => releventTrips().filter(trip => trip.isCompleted);
     const dispatch = useDispatch();
+    const nav = useNavigate();
+    useEffect(() => {
+        dispatch(findAllTripsThunk())
+    }, []);
     const handleLogout = () => {
         dispatch(logoutThunk())
     }
     return(
         <div>
-        {currentUser &&
+            {tripsLoading &&
+                <div className="text-center">
+                    <img src={LoadSVG} alt="...Loading..." />
+                </div>
+            }
+        {currentUser && !tripsLoading &&
         <div className="container margin-top">
             <div className="row">
                 <div className="col">
@@ -55,11 +72,31 @@ const ProfileComponent = () => {
                 <p className="m-0 text-secondary">Born:{currentUser.dateOfBirth}</p>
                 <br></br>
                 <div>
-                    <span className="fw-bold">Favorite Parks</span>
+                    <span className="fw-bold">Trips Planned:</span>
+                    <div>
+                        <ul className="list-group mt-3">
+                            {
+                                releventTrips().map(trip =>
+                                    <li className="list-group-item" key={`${trip.parkCode}${trip.startDate}${trip.endDate}`}>
+                                        <span className="text-secondary h5" style={{"cursor": "pointer"}} onClick={() => nav(`/details/${trip.parkCode}`)}>{trip.parkName}</span> <span className="float-end text-secondary">{trip.startDate} - {trip.endDate}</span>
+                                    </li>)
+                            }
+                        </ul>
+                    </div>
                     <br></br>
-                    <span className="text-secondary">{currentUser.favoriteParks}</span>
-                    <br></br><br></br>
-                    <span className="fw-bold">TripsPlanned</span>
+                    <span className="fw-bold">Completed Trips:</span>
+                    <div>
+                        <ul className="list-group mt-3">
+                            {
+                                completedTrips().map(trip =>
+                                    <li className="list-group-item" key={`${trip.parkCode}${trip.startDate}${trip.endDate}`}>
+                                        <span className="text-secondary h5" style={{"cursor": "pointer"}} onClick={() => nav(`/details/${trip.parkCode}`)}>{trip.parkName}</span> <span className="float-end text-secondary">{trip.startDate} - {trip.endDate}</span>
+                                    </li>)
+                            }
+                        </ul>
+                        {completedTrips().length == 0 && <div className="text-secondary">No trips to show.</div>}
+                    </div>
+
                     <br></br>
                     <span className="text-secondary">{currentUser.tripsPlanned}</span>
                     <br></br><br></br>
