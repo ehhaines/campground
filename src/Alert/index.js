d1603a16562752e285eb9997eba056eb9ceb9ae2
ehhaines <haines.e@northeastern.edu>
2022-12-13 23:36:03 -0500

    Add delete functionality for rangers regarding alerts

--- src/Alert/index.js
+++ src/Alert/index.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch } from "react-redux";
+import { resolveAlertThunk } from "./alerts-thunks";
 
 const AlertItem = ({alert}) => {
+
+  const dispatch = useDispatch();
+
   return(
     <li className="list-group-item" style={{backgroundColor: 'rgba(217, 83, 79, 0.3)'}}>
       <div className="row">
-        <div className="col-6 h5 text-danger">ALERT</div>
-        <div className="col text-end me-3 pe-2">{alert.datePosted}</div>
+        <div className="col-6 h5 text-danger"><FontAwesomeIcon icon={faCircleExclamation}/> ALERT</div>
+        <div className="col text-end"><span className="text-secondary me-1">{alert.datePosted}</span> <button className="btn" onClick={() => {dispatch(resolveAlertThunk(alert._id))}}><FontAwesomeIcon icon={faX}/></button></div>
       </div>
-      <div className="mb-2">
+      <div className="mb-2 text-secondary">
         {alert.alert}
       </div>
     </li>
